7936a13368aae40ad59ded8a9963571fc872a485
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-07-27 22:19:24 +0530

    project completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import LightModeIcon from "@mui/icons-material/LightMode";
 import { Tooltip } from "@mui/material";
 const Navbar = ({ movie, setMovie }) => {
   const [search, setSearch] = useState("");
+  const [isDark, setIsDark] = useState(false);
   const handleSearch = (e) => {
     const value = e.target.value;
     setSearch(value);
@@ -26,13 +27,25 @@ const Navbar = ({ movie, setMovie }) => {
             onChange={handleSearch}
           />
         </div>
-
-        <Tooltip title="Switch to Dark Mode">
-          <DarkModeIcon className={styles.themeIcons} />
-        </Tooltip>
-        {false && (
+        {!isDark ? (
+          <Tooltip title="Switch to Dark Mode">
+            <DarkModeIcon
+              className={styles.themeIcons}
+              onClick={() => {
+                setIsDark(!isDark);
+                document.body.classList.add("darkMode");
+              }}
+            />
+          </Tooltip>
+        ) : (
           <Tooltip title="Switch to Light Mode">
-            <LightModeIcon className={styles.themeIcons} />
+            <LightModeIcon
+              className={styles.themeIcons}
+              onClick={() => {
+                setIsDark(!isDark);
+                document.body.classList.remove("darkMode");
+              }}
+            />
           </Tooltip>
         )}
       </div>
